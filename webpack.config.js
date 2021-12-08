723e5ce6af9f9920ea77ba55879aedc938520c99
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-12-08 22:16:27 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            plugins: [
+              ["@babel/plugin-proposal-class-properties"],
+              ["@babel/transform-runtime"],
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
