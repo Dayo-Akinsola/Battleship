5ba651b8f29c7fa7ed714891b5974894ff700316
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-12-16 18:08:21 +0000

    added methods for computer to make intelligent moves

--- src/player.js
+++ src/player.js
@@ -5,14 +5,54 @@ const Player = () => {
     return shipAttacked;
   }
 
+  const getPlayerShip = (coordinates, playerGameBoard) => {
+    const allPlacedShips = playerGameBoard.copyPlacedShips();
+    const targetShip = allPlacedShips.filter((ship) => ship.hasCoordinates(coordinates))[0];
+    return targetShip;
+  }
+
+  const rotateBoardShip = (playerGameBoard, targetShip) => {
+    targetShip.rotateShipIfPossible(playerGameBoard);
+    return targetShip;
+  }
+
   return {
     attackEnemyBoard,
+    rotateBoardShip,
+    getPlayerShip,
   }
 }
 
 const ComputerPlayer = () => {
 
-  const { attackEnemyBoard } = Player();
+  /* Used to store all the attacks made by the computer */
+  const successfulAttacks = [];
+  const adjacentAttackFunctions = [
+    (coordinates) => [coordinates[0] - 1, coordinates[1]], /* Left */
+    (coordinates) => [coordinates[0] + 1, coordinates[1]], /* Right */
+    (coordinates) => [coordinates[0], coordinates[1] - 1], /* Up */
+    (coordinates) => [coordinates[0], coordinates[1] + 1], /* Down */
+  ];
+  let index = 0;
+
+  const attackEnemyBoard = (enemyGameBoard, attackCoordinates) => {
+    const shipAttacked = enemyGameBoard.receiveAttack(attackCoordinates);
+    if (shipAttacked) {
+      successfulAttacks.push(attackCoordinates);
+    } else if (!shipAttacked) {
+      if (successfulAttacks.length > 1) {
+        successfulAttacks.splice(1);
+        index += 1;
+      } 
+    }
+
+    if (enemyGameBoard.sunkShipCheck()) {
+      successfulAttacks.splice(0);
+      index = 0;
+    }
+
+    return shipAttacked;
+  }
 
   const pickRandomCoordinates = (enemyGameBoard) => {
     let randomX = Math.floor(Math.random() * 10);
@@ -26,9 +66,41 @@ const ComputerPlayer = () => {
     return coordinates;
   }
 
+  const pickAdjacentCoordinates = (enemyGameBoard) => {
+    let lastCoordinates = successfulAttacks[successfulAttacks.length - 1];
+    let [x, y] = adjacentAttackFunctions[index](lastCoordinates);
+
+    if ( 
+      !enemyGameBoard.isPositionFreeToAttack([x, y]) 
+      || x > 9 || y > 9  
+      || x < 0 || y < 0
+      ) {
+      successfulAttacks.splice(1);
+      lastCoordinates = successfulAttacks[successfulAttacks.length - 1];
+    }
+ 
+    while ( 
+      !enemyGameBoard.isPositionFreeToAttack([x, y]) 
+      || x > 9 || y > 9 
+      || x < 0 || y < 0
+      ) {
+      index += 1;
+      [x, y] = adjacentAttackFunctions[index](lastCoordinates);
+    }
+
+    return [x, y];
+  }
+
+  const pickCoordinates = (enemyGameBoard) => {
+    if (successfulAttacks.length === 0) {
+      return pickRandomCoordinates(enemyGameBoard);
+    }
+    return pickAdjacentCoordinates(enemyGameBoard);
+  }
+
   return {
     attackEnemyBoard,
-    pickRandomCoordinates,
+    pickCoordinates,
   }
 }
 
--- src/ship.js
+++ src/ship.js
@@ -1,4 +1,4 @@
-const Ship = (length, shipCoordinates) => {
+const Ship = (length, startCoordinates, shipCoordinates) => {
   /*
     Each ship coordinate is stored as a key in the object with the 
     value being a boolean showing whether the position has been hit
@@ -18,6 +18,8 @@ const Ship = (length, shipCoordinates) => {
 
   const getShipName = () => shipNames[length];
 
+  const getShipCoordinates = () => shipCoordinates;
+
   const hasCoordinates = (attackCoordinates) => {
     let matchFound = false;
     shipCoordinates.forEach((coordinates) => {
@@ -36,15 +38,65 @@ const Ship = (length, shipCoordinates) => {
 
   const isSunk = () => {
     const shipHitBools = Object.values(shipHits);
-    return shipHitBools.every((bool) => bool === true);
+    return shipHitBools.every((bool) => bool);
   }
+
+  /* Checks if y coordinates of the ship are the same */
+  const isShipHorizontal = () => {
+    const coordinates = getShipCoordinates();
+    const [ firstCoordinates, secondCoordinates ] = coordinates;
+    if (firstCoordinates[1] === secondCoordinates[1]) {
+      return true;
+    } 
+    return false;
+  }
+
+
+  const rotateShip = () => {
+    const isHorizontal = isShipHorizontal();
+    shipCoordinates.length = 0;
+    if (isHorizontal) {
+      for (let i = 0; i < length; i += 1) {
+        shipCoordinates.push([startCoordinates[0], startCoordinates[1] + i]);
+      }
+    } else {
+      for (let i = 0; i < length; i+= 1) {
+        shipCoordinates.push([startCoordinates[0] + i, startCoordinates[1]]);
+      }
+    }
+  }
+
+  /* 
+    This function rotates a ship on the gameboard and checks if the new coordinates are possible on the gameboard.
+    If the position is possible the rotation is kept, if it is not then the ship is rotated back to its original
+    position.
+  */
+  const rotateShipIfPossible = (playerGameBoard) => {
+    let rotation = isShipHorizontal();
+    rotateShip();
+    rotation = !rotation;
+    const placedShipsCopy = playerGameBoard.copyPlacedShips();
+    /* Filters out the ship that has been rotated */
+    const filteredShips = placedShipsCopy.filter((placedShip) => placedShip.getShipCoordinates()[0] !== shipCoordinates[0] );
+    if (playerGameBoard.isPositionAvailiable(length, startCoordinates, rotation, filteredShips)) {
+      return true;
+    } 
+    rotateShip();
+    rotation = !rotation;
+    return false; 
+  }
+
+  const getShipLength = () => length;
+
 
   return {
     getShipName,
     hit,
     isSunk,
     hasCoordinates,
-    shipCoordinates,
+    getShipCoordinates,
+    rotateShipIfPossible,
+    getShipLength,
   }
 };
 
